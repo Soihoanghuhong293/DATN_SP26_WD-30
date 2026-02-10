7acff9f43a7b3b013a959e05eb41020956f9b9a9
Soihoanghuhong293 <pxq293it@gmail.com>
2026-02-10 15:43:21 +0700

    fix base develop

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,22 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+
 import connectDB from './config/database.js';
 import { AppError } from './utils/AppError.js';
-import tourRouter from './routes/tour.routes.js';
-// Import Routes
-// import tourRouter from './routes/tour.routes';
 
-import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 // 1. Middlewares
-app.use(cors()); // Cho phép Frontend gọi API
-app.use(express.json()); // Đọc JSON body
-app.use(morgan('dev')); // Log request
-app.use('/api/v1/tours', tourRouter);
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 // 2. Connect DB
 connectDB();
@@ -25,9 +22,7 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-// app.use('/api/v1/tours', tourRouter);
 
-// 4. Handle Undefined Routes
 // 4. Handle Undefined Routes
 app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
@@ -46,9 +41,3 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 export default app;
-
-// Server Listen (thường tách ra file server.ts hoặc để cuối app.ts)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
