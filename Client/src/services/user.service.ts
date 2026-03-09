c6ad264b8b6c0e93215609eacf08e3582f2e5a0e
Trinh Hung <hungtvph50920@gmail.com>
2026-03-09 22:52:56 +0700

    chi tiet sua va xoa

--- Client/src/services/user.service.ts
+++ Client/src/services/user.service.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/v1/admin";
+const API_URL = "http://localhost:5000/api/v1/admin/users";
 
 export const getAllUsers = async () => {
-  const res = await axios.get(`${API_URL}/users`);
+  const res = await axios.get(API_URL);
+  return res.data;
+};
+
+export const updateUser = async (id: string, data: any) => {
+  const res = await axios.put(`${API_URL}/${id}`, data);
+  return res.data;
+};
+
+export const deleteUser = async (id: string) => {
+  const res = await axios.delete(`${API_URL}/${id}`);
   return res.data;
 };
